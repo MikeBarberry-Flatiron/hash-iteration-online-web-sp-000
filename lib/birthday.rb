79841a91db74014b6f03b416026699d17ffe987e
Mike Barberry <mikebarberry@protonmail.com>
2020-10-09 19:57:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,3 +11,13 @@ def happy_birthday(birthday_kids)
 end
 
 happy_birthday(birthday_kids)
+
+def age_appropriate_birthday(array)
+  birthday_kids.each do |name, age|
+    if age < 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else 
+      puts "You are too old for this."
+    end 
+  end
+end 
