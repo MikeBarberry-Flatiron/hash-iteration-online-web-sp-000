78660b480f049a8631c26486e108bb88600bedb7
Mike Barberry <mikebarberry@protonmail.com>
2020-10-09 19:58:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -18,6 +18,6 @@ def age_appropriate_birthday(array)
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     else
       puts "You are too old for this."
-    end 
+    end
   end
 end
